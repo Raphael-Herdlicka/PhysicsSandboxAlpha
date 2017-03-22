a337c6066c0556d7329e13cd7669f8d3180233f0
Raphael Herdlicka <raphael.herdlicka@gmail.com>
2017-03-22 08:33:36 +0100

    Added BrushSize to DestroyTool

--- Assets/Scripts/DestroyTool.cs
+++ Assets/Scripts/DestroyTool.cs
@@ -4,10 +4,38 @@ namespace Assets.Scripts
 {
     public class DestroyTool : RapidFiringTool
     {
+        [Header("Specific Settings")]
+        [SerializeField]
+        private float brushSize = 1;
+
+        protected override void DoUpdate()
+        {
+            base.DoUpdate();
+            if (Input.GetKeyDown(KeyCode.Equals))
+            {
+                brushSize++;
+            }
+            if (Input.GetKeyDown(KeyCode.Minus) && brushSize > 0)
+            {
+                brushSize--;
+            }
+        }
 
         protected override void DoRapidFireUpdate(RaycastHit hit)
         {
-            if (hit.transform.tag == "CreatedByPlayer")
+            if (brushSize > 1)
+            {
+                Collider[] colliders = Physics.OverlapSphere(hit.point, brushSize);
+
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].tag == "CreatedByPlayer")
+                    {
+                        Destroy(colliders[i].gameObject);
+                    }
+                }
+            }
+            else if (hit.transform.tag == "CreatedByPlayer")
             {
                 Destroy(hit.transform.gameObject);
             }
